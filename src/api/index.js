378231841678a034260b9dd5c4bbe969a0caf91b
jarvis-exes <8696771553shubham@gmail.com>
2023-11-25 11:18:51 +0530

    Project Setup

---
+++ src/api/index.js
@@ -0,0 +1,11 @@
+const customeFetch = () => {};
+
+const getPosts = (page, limit) => {
+  fetch();
+  return customeFetch();
+};
+
+const createPosts = () => {
+  fetch();
+  return customeFetch();
+};
